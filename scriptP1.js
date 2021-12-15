753bf757be6ce6e413a6cbff45786a232622798f
NuTz <56292347+62070060@users.noreply.github.com>
2021-12-15 09:45:05 +0700

    Add files via upload

--- scriptP1.js
+++ scriptP1.js
@@ -1,14 +1,13 @@
-
 function NuTz(){
     newY = window.scrollY;
     document.body.style.setProperty('--h',newY*0.3);
   
     theY.innerText = newY;
     nongwow = wow.offsetTop;
-    if(newY+window.innerHeight-(wow.offsetHeight/3) >= nongwow && newY < nongwow+950){
-      wow.style.transform = 'translate(2%)';
+    if(newY+window.innerHeight-(wow.offsetHeight/3) >= nongwow && newY < nongwow+750){
+      wow.style.transform = 'translate(0%)';
     }else{
-      wow.style.transform = 'translate(150%)';
+      wow.style.transform = 'translate(200%)';
     }
 
     nongwow = wow2.offsetTop;
@@ -19,3 +18,39 @@ function NuTz(){
       wow2.style.transform = 'translate(300%)';
     }
   };
+
+function toggle1(){
+  var x = document.getElementById("U3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function toggle2(){
+  var x = document.getElementById("U2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function toggle3(){
+  var x = document.getElementById("U1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function rain(){
+  var x = document.getElementById("U8");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
